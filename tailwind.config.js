d5e8aa3520be05f06e0ccca7c0eb1612bfa38c52
Ormon-huehuehue <armnvrma10@gmail.com>
2025-01-16 20:40:19 +0530

    celeb section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,13 @@ module.exports = {
         grayText : "#8f8f8f",
         oceanBlue : "#cbe9e7",
         textPurple : "#3f378b",
-        darkPurple : "#2a2553"
+        darkPurple : "#2a2553",
+        oceanbg : "#e5f4f3"
 
       },
       fontFamily: {
         fraunces: ["Fraunces", 'sans-serif'], 
+        handlee: ['Handlee', 'cursive'],
       },
     },
   },
